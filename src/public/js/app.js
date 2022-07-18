24dfc726ac0d607e96559232634aac09d5dc841c
Uddiyam <97297404+Uddiyam@users.noreply.github.com>
2022-07-18 14:42:51 +0900

    Zoom

---
+++ src/public/js/app.js
@@ -0,0 +1,58 @@
+/*
+const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
+function makeMessage(type, payload){
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+})
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server");
+})
+
+function handleSubmit(event) {
+    event.preventDefault();     // 이벤트를 취소하는 메서드, 이벤트가 제공하는 원래 기능을 사용하지 않고자 할 때 주로 사용
+    const input = messageForm.querySelector('input');
+    socket.send(makeMessage('new_message', input.value));
+    input.value="";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector('input');
+    socket.send(makeMessage('nickname', input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
+*/
+
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import http from "http";
+// import WebSocket from 'ws';
+import SocktIO from "socket.io";
+
+const app = express(); // expres는 HTTP 기반으로 동작
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); //__dirname은 Node.js의 기본 전역 변수, 현재 실행하는 폴더의 경로
+
+app.use("/public", express.static(__dirname + "/public"));
+
+// app.get 메서드 -> 서버에 HTTP 요청이 왔을 때 지정된 콜백을 이용하여 라우팅 처리 해줌
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocktIO(httpServer);
+
+/*const wss = new WebSocket.Server({ server });   // 서버에 웹소켓 프로토콜 추가 => HTTP 서버는 사용자에게 뷰 엔진을 이용해 만든 뷰, 정적 파일, 리다이렉션 등 제공, 웹소켓 서버는 실시간 채팅 기능 제공
+
+const sockets = [];
+
+wss.on("connection",(socket) => {
+    sockets.push(socket);
+    socket['nickname'] = 'Anonymous';
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message",(msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
+                break;
+            case "nickname":
+                socket['nickname'] = message.payload;
+                break;
+        }
+    });
+})
+*/
+
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    console.log(socket.id);
+    console.log(socket.rooms);
+    socket.join(roomName);
+    console.log(socket.rooms);
+  });
+});
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
